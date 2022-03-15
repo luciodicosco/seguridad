0bf75ad10b270d51874612902e37ea22b991d44f
luciodicosco <dicoscolucho@gmail.com>
2022-03-14 23:44:38 -0300

    se pulieron detalles

--- js/app.js
+++ js/app.js
@@ -1,21 +1,17 @@
 var menulateral = false
 
-$(document).ready(function () {
-    if (menulateral == false) {
-        console.log("menu cerrado")
-    };
-    if (menulateral == true) {
-        console.log("menu abierto")
-    };
-}
-)
-
 $(function () {
     $('#hambButton').click(function () {
-        $('.menuContenedor').slideDown();
-    })
-
-    $('#closeButton').click(function () {
-        $('.menuContenedor').slideUp();
+        checkHambMenu()
     })
 })
+
+function checkHambMenu() {
+    if (menulateral == true){
+        $('.menuContenedor').slideDown();
+        menulateral = false
+    } else {
+        $('.menuContenedor').slideUp();
+        menulateral = true
+    }
+}
