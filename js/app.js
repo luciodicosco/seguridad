a6ca16a95c0c8453e8e7ad34c81df57550f813cf
luciodicosco <dicoscolucho@gmail.com>
2022-03-14 21:35:47 -0300

    se borro bootstrap

---
+++ js/app.js
@@ -0,0 +1,8 @@
+$(".hambButton").click(function () {
+    $(".menuContenedor").animate({ width: '100px' });
+})
+
+
+$(".closeButton").click(function () {
+    $(".menuContenedor").animate({ width: '0px' });
+})
